b7fbeb6679b3ea1a63c471bc15ade099eab322a8
kapilreddysamagit <kapilreddy.sama@gmail.com>
2014-07-14 18:34:53 +0530

    test
    dsfasf

--- src/PartnerSamples.java
+++ src/PartnerSamples.java
@@ -15,7 +15,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.*; //test push
+import java.util.*; //test push test
 
 public class PartnerSamples {
     PartnerConnection partnerConnection = null;
